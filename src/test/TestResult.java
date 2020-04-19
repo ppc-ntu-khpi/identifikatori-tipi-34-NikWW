26d114caa81c5d2c60ce865e493c9ea1409308b1
NikWiseUa <bibicon228@gmail.com>
2020-04-19 17:08:12 +0300

    End

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,24 @@
 package test;
 
 import domain.Exercise;
-
+/**
+ *  Клас который подсчитывает и выводит данные и содержит реализацию методов, запускается при запуске программы
+ * @author Lenovo
+ *
+ */
 public class TestResult {
-
+	/**
+	 * Метод запускается при запуске программы и подсчитывает все делители входящего числа
+	 * @param args указывает на то что нужно запустить при запуске программы
+	 */
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+    	/**
+    	 *  Bходное число
+    	 */
+        int R = 999999; 
+        /**
+         *  В класе Exercise выполнить Calculate(вывод данных)
+         */
+        System.out.println(Exercise.Calculate(R));
     }
 }
